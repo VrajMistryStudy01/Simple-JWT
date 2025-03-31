c248ff3fe3b8977c1805d7d0d0419eb505a34c27
VrajMistryStudy01 <vrajstudyemail@gmail.com>
2025-03-31 21:22:36 +0530

    Added RegisterSerializer and RegisterView for user registration with JWT token

--- jwt_practice/accounts/views.py
+++ jwt_practice/accounts/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework_simplejwt import RefreshToken
+from django.contrib.auth import get_user_model
+from .serializers import RegisterSerializer
 
-# Create your views here.
+User =  get_user_model()
+
+class RegisterView(APIView):
+    def post(self, request):
+        serializer =  RegisterSerializer(data = request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+        
+            refreshToken = RefreshToken.for_user(user)
+            accessToken = str(refreshToken.access_token)
+
+            return Response(
+            {
+                'Message':"Creation Success",
+                "User_access_Token" : accessToken
+            },
+            status= status.HTTP_201_CREATED
+        )
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
